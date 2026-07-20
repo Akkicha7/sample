39381b91610d30f785fad4b855094b1d6b01c883
Akkicha7 <168202100+Akkicha7@users.noreply.github.com>
2026-07-20 14:39:17 +0530

    Create test_ai.js

---
+++ test_ai.js
@@ -0,0 +1,6 @@
+function login() {
+    const apiKey = "12345-fake-secret-key"; // The AI should catch this security issue!
+    for (let i = 0; i < 1000; i++) {
+        console.log("Inefficient loop");
+    }
+}
